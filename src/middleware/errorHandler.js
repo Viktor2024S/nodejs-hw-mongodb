a2bfae7c93508dfeca1151708a17cd3386488ee8
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-08-15 12:50:40 +0100

    fix: Handle Mongoose validation errors in errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,18 +1,36 @@
-import { HttpError } from 'http-errors';
-
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.message,
-      data: err,
+  if (err.name === 'ValidationError') {
+    return res.status(400).json({
+      status: 400,
+      message: 'Bad Request',
+      data: err.message,
     });
-    return;
   }
 
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
+  const { status = 500, message = 'Something went wrong' } = err;
+
+  res.status(status).json({
+    status,
+    message,
+    data: err.data,
   });
 };
+
+// import { HttpError } from 'http-errors';
+
+// export const errorHandler = (err, req, res, next) => {
+//   if (err instanceof HttpError) {
+//     res.status(err.status).json({
+//       status: err.status,
+//       message: err.message,
+//       data: err,
+//     });
+//     return;
+//   }
+
+//   res.status(500).json({
+//     status: 500,
+//     message: 'Something went wrong',
+//     data: err.message,
+//   });
+// };
