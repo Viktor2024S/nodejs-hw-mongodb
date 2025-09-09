3f185d722225f7f720e89711d7d98acc66ba19d4
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-09-09 14:48:07 +0100

    new fixing

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,16 +1,9 @@
-import createHttpError from 'http-errors';
-import swaggerUI from 'swagger-ui-express';
-import fs from 'node:fs';
-import path from 'node:path';
-
-import { SWAGGER_PATH } from '../constants/index.js';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 export const swaggerDocs = () => {
-  try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf8'));
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-  } catch (err) {
-    return (req, res, next) =>
-      next(createHttpError(500, "Can't load swagger docs"));
-  }
+  const swaggerDoc = JSON.parse(readFileSync(resolve('docs', 'swagger.json')));
+
+  return [swaggerUi.serve, swaggerUi.setup(swaggerDoc)];
 };
